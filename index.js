ebb322d82696e2661c0a027e3fb203597be59c53
Sabbirahmed0007 <>
2024-01-08 00:57:41 +0600

    if else statement

--- index.js
+++ index.js
@@ -75,11 +75,88 @@ friends.splice(1, 2); // handy to remove certain elements
 // console.log(friends);
 
 
+// --------------------------------------
+// If Else Statement
+//---------------------------------------
+
+//  Am I adult?
+
+var age = 25;
+
+if (age>=18) {
+    console.log(' Yes, I am Adult');
+} else {
+    console.log('No, I am not Adult');
+}
 
 
+//  Did I pass or Fail?
+
+var marks = 30;
+
+if (marks>= 80) {
+    console.log('You Got A+');
+} 
+else if(marks>=70){
+    console.log('You got A');
+}
+else if(marks>=60 ){
+    console.log('You got A-');
+}
+else if(marks>=50 ){
+    console.log('You got B');
+}
+else if(marks>=33 ){
+    console.log('You got C');
+}
+else {
+    console.log('You are Fail');
+}
+// if (marks>= 80) {
+//     console.log('You Got A+');
+// } 
+// else if(marks>=70 && marks<79){
+//     console.log('You got A');
+// }
+// else if(marks>=60 && marks<69){
+//     console.log('You got A-');
+// }
+// else if(marks>=50 && marks<59){
+//     console.log('You got B');
+// }
+// else if(marks>=33 && marks<49){
+//     console.log('You got C');
+// }
+// else {
+//     console.log('You are Fail');
+// }
 
 
+// -------------------------------------
+// Nested if Else Statement
+//--------------------------------------
 
+
+var temperature = 23 ;
+var isRaining = true;
+
+if (temperature > 30) {
+    console.log('It is a hot sunny day.');
+    if(isRaining === true){
+        console.log('But It is raining.');
+    }
+    else{
+        console.log('And It is not raining.');
+    }
+}
+else {
+    console.log('It is a cold day.');
+    if (isRaining) {
+        console.log("And It's raining.");
+    } else {
+        console.log('But It  is not raining.');
+    }
+}
 
 
 
